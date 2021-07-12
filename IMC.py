8a6b5faed3db9da8f424f64df03ad11adee5df82
Lenner Coutinho Pereira <coutinholenner@gmail.com>
2021-07-12 19:50:44 -0300

    with dicts

--- IMC.py
+++ IMC.py
@@ -16,16 +16,13 @@ class PessoaIMC:
 
     def resultIMC(self, nome, data):
         imc = self.calculaIMC()
-        print(imc)
-        valores = [16.0, 17.0, 18.50, 25.0, 30.0, 35.0, 39.99]
-        situacoes = ["baixo peso grau II", "baixo peso grau I", "peso ideal", "sobrepeso", "obesidade grau I", "obesidade grau II"]
-        for valor in range(len(valores) -1):
-            if valores[valor] <= imc < valores[valor+1]:
-                situacao = situacoes[valor]
+        valores = {16.0: "baixo peso grau II", 17.0: "baixo peso grau I", 18.50: "peso ideal", 25.0: "sobrepeso", 30.0: "obesidade grau I", 35.0: "obesidade grau II", 39.99: None}
+        for valor in range(len(valores.keys()) -1):
+            if list(valores.keys())[valor] <= imc < list(valores.keys())[valor + 1]:
+                situacao = valores[list(valores.keys())[valor]]
                 break
         else:
-            return "Margem de valores não aceita"
-
+            return 'Margem de valores não aceita'
         return f"Nome: {nome} \nData de Nascimento: {data} \nPeso: {self.peso} \nAltura: {self.altura} \nSituação: {situacao} "
 
 # nome = input("Qual o seu nome? \nR: ")
@@ -34,4 +31,4 @@ class PessoaIMC:
 # altura = float(input("Qual a sua altura? \nR:"))
 
 troxa = PessoaIMC(60, 1.83)
-print(troxa.resultIMC('lenner', '28/12/2003'))
+print(troxa.resultIMC('Lenner', '28/12/2003'))
